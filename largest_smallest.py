f34d9c14293c76840443d2d8250b48caac691c51
Sneha Kumari <dimpi.sneha@gmail.com>
2026-09-22 15:51:55 +0530

    add a new file

---
+++ largest_smallest.py
@@ -0,0 +1,14 @@
+arr = [12, 5, 78, 34, 9, 56]
+
+largest = arr[0]
+smallest = arr[0]
+
+for i in arr:
+    if i > largest:
+        largest = i
+
+    if i < smallest:
+        smallest = i
+
+print("Maximum =", largest)
+print("Minimum =", smallest)
